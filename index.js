0afc8b5151a85fcb4fc14454b3fad699dbfcaa6a
Chris Lount <chris@lount.info>
2021-06-30 12:27:26 +0100

    Serve index (#7)
    * Provide a home page with map
    
    * Simple home page with map

---
+++ index.js
@@ -0,0 +1,37 @@
+// NTISMapTools - Map interface to interact with NTIS data
+// Copyright (C) 2021 Highways England
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"use strict";
+
+const express = require("express");
+const compression = require("compression");
+
+const app = express();
+
+app.use(compression());
+app.use(express.static("static"));
+
+const port = process.env.PORT | 8000;
+
+app.set("view engine", "pug");
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    //console.log("Started app");
+});
---
+++ static/scripts/map.js
@@ -0,0 +1,28 @@
+// NTISMapTools - Map interface to interact with NTIS data
+// Copyright (C) 2021 Highways England
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"use strict";
+
+/* global L */
+
+(function(){
+
+    const map = L.map("NTISMap").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+    }).addTo(map);
+})();
